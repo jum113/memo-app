d9e734699851b580c9ab08a6315f8f6c9c7af2ef
jum113 <japan_movieact@yahoo.co.jp>
2022-01-20 08:12:41 +0900

    日時の追加表示と日本時刻への変更

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -2,9 +2,16 @@ const buildHTML = (XHR) => {
   const item = XHR.response.post;
   const html = `
     <div class="card mx-4 my-3 " style="width: 20rem;">
-      <div class="body overflow-scroll" style="height: 300px">
-        <h5 class="card-title shadow ms-2">${ item.title }</h5>
-        <p class="card-text px-2 py-3 overflow-scroll">${ item.content }</p>
+      <div class="card-body overflow-scroll" style="height: 300px">
+        <h5 class="card-title shadow ms-2">
+          ${ item.title }
+        </h5>
+        <h6 class="d-flex justify-content-end pt-2">
+          ${ item.created_at }
+        </h6>
+        <p class="card-text overflow-scroll">
+          ${ item.content }
+        </p>
       </div>
     </div>`;
   return html;
