1773ee348cca3ec2cff2cabe1d4777ff92bc12ea
jum113 <japan_movieact@yahoo.co.jp>
2022-01-20 07:34:25 +0900

    rubocopの実施

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,15 +1,16 @@
 class MemosController < ApplicationController
   def index
-    @memos = Memo.order("created_at DESC")
+    @memos = Memo.order('created_at DESC')
     @memo = Memo.new
   end
 
   def create
     memo = Memo.create(memo_params)
-    render json:{ post: memo }
+    render json: { post: memo }
   end
 
   private
+
   def memo_params
     params.require(:memo).permit(:title, :content).merge(user_id: current_user.id)
   end
