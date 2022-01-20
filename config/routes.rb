274beeeb2e7a433e0c16a3d0b48e3f2d26c18511
jum113 <japan_movieact@yahoo.co.jp>
2022-01-20 10:19:16 +0900

    同期通信によるメモ削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'memos#index'
-  resources :memos, only: [:index, :create]
+  resources :memos, only: [:index, :create, :destroy]
 end
